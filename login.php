9bd1856adf37ff3441328cd9cd54d7824f008e34
warriortom123123 <taigaaisaka192002@gmail.com>
2024-03-12 14:26:30 +0700

    7

--- login.php
+++ login.php
@@ -1,5 +1,18 @@
-<form action="login.php" method="post">
-    Tên Đăng Nhập: <input type="text" name="username"><br>
-    Mật Khẩu: <input type="password" name="password"><br>
-    <input type="submit" value="login">
-</form>
+<?php
+session_start();
+
+if(isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    // Check login credentials
+    if($username === 'admin' && $password === 'admin') {
+        $_SESSION['username'] = $username;
+        $_SESSION['login_success'] = true; // Setting a session variable to indicate login success
+        header('Location: student-list.php');
+        exit();
+    } else {
+        echo "Tên đăng nhập hoặc mật khẩu không đúng.";
+    }
+}
+?>
