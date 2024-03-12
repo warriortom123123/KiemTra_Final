2257c46d1c485080ac61d717412862123fa075df
locnguyen1005 <nguyenvanloc12a3nh2020@gmail.com>
2024-03-12 14:43:23 +0700

    Revert "3"
    This reverts commit 630f4cd018ffe770c662627b2717d21d2c700d0f.

--- login.php
+++ login.php
@@ -1,18 +1,3 @@
-<?php
-session_start();
-
-if(isset($_POST['username']) && isset($_POST['password'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    
-    // Check login credentials
-    if($username === 'admin' && $password === 'admin') {
-        $_SESSION['username'] = $username;
-        $_SESSION['login_success'] = true; // Setting a session variable to indicate login success
-        header('Location: studentlist.php');
-        exit();
-    } else {
-        echo "Tên đăng nhập hoặc mật khẩu không đúng.";
-    }
-}
-?>
+<form action="login.php" method="post">
+    Tên đăng nhập: <input type="text" name="username"><br>
+    password: <input type="password" name="password"><br>
